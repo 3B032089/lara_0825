f77ee1e2d4757005c2fe82d66020fc53b0308e4d
Fuuzuki0307 <Yaesakura716207@gmail.com>
2023-11-29 00:18:04 +0800

    使用save方法

--- routes/web.php
+++ routes/web.php
@@ -36,9 +36,14 @@ Route::get('/', function () {
     dd($posts);
     */
     $post = Post::find(1);
+    /*
     $post->update([
         'title' => 'update title',
         'content' => 'update content',
     ]);
+    */
+    $post->title = 'saved title';
+    $post->content = 'saved content';
+    $post->save();
     return view('welcome');
 });
